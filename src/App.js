32dbdb7adc7b809ed0dcb03d119dda32ae05e93a
Roman De Rose <romandr37@gmail.com>
2022-05-10 00:56:23 -0300

    funcionalidad de adivinar la palabra

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import { useState, useRef, useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { words } from "./utils/words";
 
 function App() {
-  const WORDS = [
-    "FORMA",
-    "HIELO",
-    "CIELO",
-    "SELVA",
-    "ROCAS",
-    "BAÑOS",
-    "CAJAS",
-    "DADOS",
-    "DONDE",
-    "CONDE",
-    "MUCHO",
-    "ESTAR",
-    "RANGO",
-    "METRO",
-    "MICRO",
-    "ACTOR",
-  ];
   const [word, setWord] = useState("");
   const [attemps, setAttemps] = useState([]);
   const [lifes, setLifes] = useState(5);
+  const [reset, setreset] = useState(false);
   const wordInput = useRef(null);
 
   const handleSubmitWord = (e) => {
     e.preventDefault();
     setAttemps([...attemps, wordInput.current.value.toUpperCase().split()]);
     setLifes(lifes - 1);
-    e.target.firstChild.value = "";
+    if (wordInput.current.value.toUpperCase() === word) {
+      setreset(true);
+      toast.success("Acertaste!");
+    }
+    wordInput.current.value = "";
   };
+
+  const reload = () => document.location.reload();
 
   const styleLetterConditional = (wordUser, index, letter) => {
     if (word.includes(letter)) {
@@ -45,16 +36,14 @@ function App() {
   console.log(word);
 
   useEffect(() => {
-    const randomPick = Math.floor(Math.random() * WORDS.length);
-    setWord(WORDS[randomPick]);
+    const randomPick = Math.floor(Math.random() * words.length);
+    setWord(words[randomPick]);
   }, []);
 
   return (
-    <div className="min-h-screen bg-zinc-900 py-4">
-      <h1 className="text-yellow-500 text-center font-extrabold text-5xl mb-1">
-        WORDLE
-      </h1>
-      <p className="text-yellow-500 text-center text-lg mb-3">
+    <div className="w-full min-h-screen text-center bg-zinc-900 py-4">
+      <h1 className="text-yellow-500  font-extrabold text-5xl mb-1">WORDLE</h1>
+      <p className="text-yellow-500  text-lg mb-3">
         Tenés{" "}
         <span className="text-yellow-300 font-extrabold text-xl">{lifes}</span>{" "}
         intentos para adivinar la palabra!
@@ -127,27 +116,52 @@ function App() {
           </div>
         ))}
       </div>
-      <form onSubmit={handleSubmitWord} className="w-52 h-auto mx-auto mt-8">
-        <input
-          ref={wordInput}
-          type="text"
-          placeholder="Palabra..."
-          className="w-full bg-transparent border-4 border-yellow-500 rounded-2xl p-2 text-md text-white placeholder:text-amber-200 active:outline-none focus:outline-none"
-          maxLength="5"
-          required
-        />
-      </form>
-      <div className="my-4 w-full text-center">
+      {reset === false ? (
+        <form onSubmit={handleSubmitWord} className="w-52 h-auto mx-auto mt-8">
+          <input
+            ref={wordInput}
+            type="text"
+            placeholder="Palabra..."
+            className="w-full bg-transparent border-4 border-yellow-500 rounded-2xl p-2 text-md text-white placeholder:text-amber-200 active:outline-none focus:outline-none"
+            maxLength="5"
+            minLength="5"
+            required
+          />
+        </form>
+      ) : (
         <button
-          onClick={handleSubmitWord}
-          className="bg-sky-500 mx-2 py-2 px-3 rounded-2xl text-md uppercase text-white font-semibold hover:shadow-lg hover:shadow-sky-500/30"
+          onClick={reload}
+          className=" bg-sky-500 mx-auto px-4 py-2 text-white uppercase font-bold text-xl rounded-2xl hover:shadow-lg hover:shadow-sky-500/30"
         >
-          Revisar
+          nueva palabra
         </button>
-        <button className="bg-sky-500 mx-2 py-2 px-3 rounded-2xl text-md uppercase text-white font-semibold hover:shadow-lg hover:shadow-sky-500/30">
-          omitir
-        </button>
-      </div>
+      )}
+      {reset === false && (
+        <div className="my-4 w-full text-center">
+          <button
+            onClick={handleSubmitWord}
+            className="bg-sky-500 mx-2 py-2 px-3 rounded-2xl text-md uppercase text-white font-semibold hover:shadow-lg hover:shadow-sky-500/30"
+          >
+            Revisar
+          </button>
+        </div>
+      )}
+
+      <Toaster
+        toastOptions={{
+          position: "top-center",
+          duration: 2500,
+          style: {
+            backgroundColor: "#EAB308",
+            color: "#fff",
+            fontSize: "1.4rem",
+            fontWeight: "bold",
+            textTransform: "uppercase",
+            borderRadius: "18px",
+            padding: "12px",
+          },
+        }}
+      />
     </div>
   );
 }
---
+++ src/utils/words.js
@@ -0,0 +1,20 @@
+const words = [
+  "FORMA",
+  "HIELO",
+  "CIELO",
+  "SELVA",
+  "ROCAS",
+  "BAÑOS",
+  "CAJAS",
+  "DADOS",
+  "DONDE",
+  "CONDE",
+  "MUCHO",
+  "ESTAR",
+  "RANGO",
+  "METRO",
+  "MICRO",
+  "ACTOR",
+];
+
+export { words };
