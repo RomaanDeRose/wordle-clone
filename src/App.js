ddcee85f33a3e9b273ba73928a99253139d03ea5
Roman De Rose <romandr37@gmail.com>
2022-05-18 17:05:56 -0300

    animaciones, instalacion react-router, etc.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { useState, useRef, useEffect } from "react";
+import { motion } from "framer-motion";
 import toast, { Toaster } from "react-hot-toast";
 import { words } from "./utils/words";
 
 function App() {
   const [word, setWord] = useState("");
   const [attemps, setAttemps] = useState([]);
-  const [lifes, setLifes] = useState(5);
+  const [lifes, setLifes] = useState(6);
   const [reset, setReset] = useState(false);
   const [result, setResult] = useState("");
   const wordInput = useRef(null);
 
   const handleSubmitWord = (e) => {
     e.preventDefault();
+    setAttemps([...attemps, wordInput.current.value.toUpperCase().split()]);
+    setLifes(lifes - 1);
     if (lifes > 1) {
-      setAttemps([...attemps, wordInput.current.value.toUpperCase().split()]);
-      setLifes(lifes - 1);
       if (wordInput.current.value.toUpperCase() === word) {
         setReset(true);
         setResult("win");
@@ -35,21 +36,26 @@ function App() {
     }
     wordInput.current.value = "";
   };
+  console.log(attemps);
 
   const reload = () => document.location.reload();
 
-  const styleLetterConditional = (wordUser, index, letter) => {
+  const styleLetterConditional = (wordUser = undefined, index, letter) => {
     if (word.includes(letter)) {
-      if (word.includes(letter) && word[index] === wordUser[index]) {
+      if (word[index] === wordUser[index]) {
         return "bg-green-500";
       } else {
         return "bg-yellow-500";
       }
     } else {
+      if (wordUser === undefined) {
+        return "bg-white";
+      }
       return "bg-gray-500";
     }
   };
-  console.log(word);
+
+  console.log(attemps[0]);
 
   useEffect(() => {
     const randomPick = Math.floor(Math.random() * words.length);
@@ -58,16 +64,31 @@ function App() {
 
   return (
     <div className="w-full min-h-screen text-center bg-zinc-900 py-4">
-      <h1 className="text-yellow-500  font-extrabold text-5xl mb-1">WORDLE</h1>
-      <p className="text-yellow-500  text-lg mb-3">
+      <motion.h1
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.3, type: "fade" }}
+        className="text-yellow-400  font-extrabold text-5xl mb-1"
+      >
+        WORDLE
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.3, delay: 0.2, type: "fade" }}
+        className="text-yellow-500  text-lg mb-3"
+      >
         Tenés{" "}
         <span className="text-yellow-300 font-extrabold text-xl">{lifes}</span>{" "}
         intentos para adivinar la palabra!
-      </p>
+      </motion.p>
       <div className="w-4/5 max-w-xs h-auto mx-auto">
         {attemps.map((attemp, index) => (
           <div key={index} className="w-full h-10 mb-5 flex gap-2">
-            <p
+            <motion.p
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.4 }}
               className={`w-1/5 h-full border-2 ${styleLetterConditional(
                 attemp[0],
                 0,
@@ -75,8 +96,11 @@ function App() {
               )} border-gray-700 text-white font-bold text-xl rounded-lg text-center leading-9`}
             >
               {attemp[0][0]}
-            </p>
-            <p
+            </motion.p>
+            <motion.p
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5, delay: 0.3 }}
               className={`w-1/5 h-full border-2 ${styleLetterConditional(
                 attemp[0],
                 1,
@@ -84,8 +108,11 @@ function App() {
               )} border-gray-700 text-white font-bold text-xl rounded-lg text-center leading-9`}
             >
               {attemp[0][1]}
-            </p>
-            <p
+            </motion.p>
+            <motion.p
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5, delay: 0.8 }}
               className={`w-1/5 h-full border-2 ${styleLetterConditional(
                 attemp[0],
                 2,
@@ -93,8 +120,11 @@ function App() {
               )} border-gray-700 text-white font-bold text-xl rounded-lg text-center leading-9`}
             >
               {attemp[0][2]}
-            </p>
-            <p
+            </motion.p>
+            <motion.p
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5, delay: 1.2 }}
               className={`w-1/5 h-full border-2 ${styleLetterConditional(
                 attemp[0],
                 3,
@@ -102,8 +132,11 @@ function App() {
               )} border-gray-700 text-white font-bold text-xl rounded-lg text-center leading-9`}
             >
               {attemp[0][3]}
-            </p>
-            <p
+            </motion.p>
+            <motion.p
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5, delay: 1.6 }}
               className={`w-1/5 h-full border-2 ${styleLetterConditional(
                 attemp[0],
                 4,
@@ -111,22 +144,29 @@ function App() {
               )} border-gray-700 text-white font-bold text-xl rounded-lg text-center leading-8`}
             >
               {attemp[0][4]}
-            </p>
+            </motion.p>
           </div>
         ))}
       </div>
       {reset === false ? (
-        <form onSubmit={handleSubmitWord} className="w-52 h-auto mx-auto mt-8">
+        <motion.form
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.3, delay: 0.5, type: "fade" }}
+          onSubmit={handleSubmitWord}
+          className="w-52 h-auto mx-auto mt-8"
+        >
           <input
             ref={wordInput}
             type="text"
             placeholder="Palabra..."
-            className="w-full bg-transparent border-4 border-yellow-500 rounded-2xl p-2 text-md text-white placeholder:text-amber-200 active:outline-none focus:outline-none"
+            className="w-full bg-transparent border-4 border-yellow-500 rounded-2xl p-2 text-md text-yellow-200 placeholder:text-amber-200/60 active:outline-none focus:outline-none"
             maxLength="5"
             minLength="5"
+            autoFocus
             required
           />
-        </form>
+        </motion.form>
       ) : (
         <button
           onClick={reload}
@@ -136,26 +176,40 @@ function App() {
         </button>
       )}
       {reset === false && (
-        <div className="my-4 w-full text-center">
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.3, delay: 0.8, type: "fade" }}
+          className="my-4 w-full text-center"
+        >
           <button
             onClick={handleSubmitWord}
             className="bg-sky-500 mx-2 py-2 px-3 rounded-2xl text-md uppercase text-white font-semibold hover:shadow-lg hover:shadow-sky-500/30"
           >
             Revisar
           </button>
-        </div>
+        </motion.div>
       )}
       {result === "win" ? (
-        <h1 className="text-yellow-500  font-extrabold text-3xl mt-4">
+        <motion.h1
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          className="text-yellow-400  font-extrabold text-3xl mt-4"
+        >
           Felicitaciones!
-        </h1>
+        </motion.h1>
       ) : result === "lose" ? (
-        <h1 className="text-yellow-500  font-extrabold text-3xl mt-4">
+        <motion.h1
+          initial={{ x: -200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.7 }}
+          className="text-yellow-400  font-extrabold text-3xl mt-3"
+        >
           La palabra era:{" "}
-          <span className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-sky-400 to-sky-700">
+          <span className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-orange-400">
             {word}
           </span>
-        </h1>
+        </motion.h1>
       ) : (
         ""
       )}
@@ -169,7 +223,7 @@ function App() {
             fontSize: "1.4rem",
             fontWeight: "bold",
             textTransform: "uppercase",
-            borderRadius: "18px",
+            borderRadius: "14px",
             padding: "12px",
           },
         }}
